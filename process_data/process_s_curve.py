082be386df3e91de43a91111efc0c5968fc8cebf
EverardoG <everardo.a.gonzalez@gmail.com>
2021-09-22 19:38:04 -0700

    cleaner cob-com plot

--- process_data/process_s_curve.py
+++ process_data/process_s_curve.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-from typing import Dict, List
+from typing import Dict, List, Optional
 from utils import get_metrics_from_folder, METRICS
 from pathlib import Path
 from functools import cmp_to_key
@@ -67,12 +67,12 @@ def get_results_for_metric(metric_name: str, metrics_dict: Dict, ks: List, offse
         good_ks_dict[offset] = good_ks
     return offset_to_metric, good_ks_dict, sorted_ks, sorted_offsets
 
-def plot_cob_com_comparison(metrics_dict: Dict, ks: List, offsets: List, show: bool = True):
+def plot_cob_com_comparison(metrics_dict: Dict, ks: List, offsets: List, show: bool = True, save_dir: Optional[str] = None):
     offset_to_cob, good_ks_dict, _, sorted_offsets = get_results_for_metric("cob", metrics_dict, ks, offsets)
     offset_to_com, _, _, _ = get_results_for_metric("com", metrics_dict, ks, offsets)
 
     # Create two plots - one for xs and one for ys
-    fig, (ax_xs, ax_ys, ax_color_com, ax_color_cob) = plt.subplots(1, 4, sharey=False, gridspec_kw={'width_ratios': [1,1,0.1,0.1]})
+    fig, (ax_xs, ax_ys, ax_color_com, ax_color_cob) = plt.subplots(1, 4, sharey=False, gridspec_kw={'width_ratios': [1,1,0.1,0.1]}, figsize=(24, 13))
 
     # Set up color maps for cob and com
     cmap_cob_name = 'Purples'
@@ -110,25 +110,6 @@ def plot_cob_com_comparison(metrics_dict: Dict, ks: List, offsets: List, show: b
     # Remove ticks
     for ax, colors, title in zip([ax_color_com, ax_color_cob], [colors_com, colors_cob], ["COM", "COBB"]):
         # Remove ticks
-        # tick_removal_axes='both'
-        # enable_right_ticks = False
-        # if ax is ax_color_cob:
-        #     tick_removal_axes='x'
-        #     enable_right_ticks = True
-        # print(tick_removal_axes)
-        # ax.tick_params(
-        #     axis='both',          # changes apply to the x-axis
-        #     which='both',      # both major and minor ticks are affected
-        #     reset=True,
-        #     bottom=False,      # ticks along the bottom edge are off
-        #     top=False,         # ticks along the top edge are off
-        #     left=False,
-        #     right=enable_right_ticks,
-        #     labelbottom=False, # labels along the bottom edge are off
-        #     labelleft=False
-        # )
-
-        # Remove ticks
         ax.set_xticks([])
         ax.yaxis.tick_right()
         ax.set_yticks([0,19])
@@ -141,48 +122,145 @@ def plot_cob_com_comparison(metrics_dict: Dict, ks: List, offsets: List, show: b
         colors_np = np.flip(np.expand_dims(np.array(colors), axis=1), axis=0)
         ax.imshow(colors_np)
         ax.set_title(title)
-    # import sys; sys.exit(0)
-    # Set up labels that show colors corresponding to offsets
-    # ax_color_cob.set_yticks([0,19])
-    # ax_color_cob.set_yticklabels(['Offset: '+str(offset) for offset in [sorted_offsets[0], sorted_offsets[-1]] ], minor=True)
-    # ax_color_cob.set_yticklabels([1.0,1.0])
-
-
-
-    # ax_color_cob.axis('off')
-
-    # Add a color bar to each subplot
-    # fig, axs_colorbar = plt.subplots(1,2)
-    # cmaps = ['Oranges', 'Purples']
-    # for cmap, ax_colorbar in zip([cmap_cob_name, cmap_com_name], axs_colorbar):
-    #     fake_mesh = ax_colorbar.pcolormesh(np.ones((2,2)),cmap = cmap)
-    #     fig.colorbar(fake_mesh, ax=ax_ys)
-
-    # Set up a color bar
-    # cb = colorbar.ColorbarBase(ax_xs, orientation = 'horizontal', cmap=cmap_cob)
-    # cob_legend = ["Offset (COBB): " + str(float(offset)/1.02) for offset in sorted_offsets]
-    # com_legend = ["Offset (COM): " + str(float(offset)/1.02) for offset in sorted_offsets]
-    # legend = cob_legend + com_legend
-    # ax_xs.legend(legend)
-    # ax_ys.legend(legend)
+    if save_dir is not None:
+        fig.savefig(save_dir+'cob-com.png')
     if show:
         plt.show()
     return fig, (ax_xs, ax_ys)
 
-def plot_metric(metric_name:str, metrics_dict: Dict, ks: List, offsets: List):
+
+def plot_cob_com_comparison_with_delta(metrics_dict: Dict, ks: List, offsets: List, show: bool = True, save_dir: Optional[str] = None, y_correction: bool = True, normalize_to_bl = True, include_island: bool = True, include_com_in_deltas: bool = True, normalize_delta=True):
+    offset_to_cob, good_ks_dict, _, sorted_offsets = get_results_for_metric("cob", metrics_dict, ks, offsets)
+    offset_to_com, _, _, _ = get_results_for_metric("com", metrics_dict, ks, offsets)
+
+    # Create two plots - one for xs and one for ys
+    fig, ((ax_xs, ax_ys, ax_color_com),(ax_xs_delta, ax_ys_delta, ax_color_cob)) = plt.subplots(2, 3, sharey=False, gridspec_kw={'width_ratios': [1,1,0.1]}, figsize=(24, 13))
+    # Set up color maps for cob and com
+    cmap_cob_name = 'BuPu'
+    cmap_com_name = 'Oranges'
+    cmap_cob = cm.get_cmap(cmap_cob_name)
+    cmap_com = cm.get_cmap(cmap_com_name)
+    color_indicies = np.linspace(0.25, 0.75, len(offsets))
+    colors_cob = [cmap_cob(color_index) for color_index in color_indicies]
+    colors_com = [cmap_com(color_index) for color_index in color_indicies]
+
+    def correct_y(arr: np.array, normalize_to_bl: bool)->None:
+        """Corrects y in place for input array of Nx2 points"""
+        arr[:,1] -= 10.2
+        arr[:,1] *= -1
+        if normalize_to_bl:
+            arr[:,1] *= (1/1.02)
+
+    # Plot the island in COM plots if specified
+    if include_island:
+        island_coords = np.array([[15.3, 6.12]])
+        if y_correction: correct_y(island_coords, normalize_to_bl)
+        ks_f = [float(k) for k in ks]
+        island_linestyle = ":"
+        island_linewidth = 1
+        ax_xs.plot([min(ks_f), max(ks_f)], np.ones((2))*island_coords[:,0], linestyle=island_linestyle, linewidth=island_linewidth)
+        ax_ys.plot([min(ks_f), max(ks_f)], np.ones(2)*island_coords[:,1], linestyle=island_linestyle, linewidth=island_linewidth)
+        ax_xs.legend(["Island Left"])
+        ax_ys.legend(["Island Bottom"])
+
+    # Plot the COM in delta plots if specified
+    if include_com_in_deltas:
+        ks_f = [float(k) for k in ks]
+        com_linestyle = ":"
+        com_linewidth = 1
+        ax_xs_delta.plot([min(ks_f), max(ks_f)], [0, 0], linestyle=com_linestyle, linewidth=com_linewidth)
+        ax_ys_delta.plot([min(ks_f), max(ks_f)], [0,0], linestyle=com_linestyle, linewidth=com_linewidth)
+        ax_xs_delta.legend(["Center of Mass"], loc='lower right')
+        ax_ys_delta.legend(["Center of Mass"], loc='lower right')
+
+    # metrics_dict[(k, offset)] = get_metrics_from_folder(Path(root))
+
+    # Plot the xs and ys of delta from COBB
+    linestyle = "."
+    markersize = 3
+    for offset, color_cob in zip(sorted_offsets, colors_cob):
+        # Turn cob results into numpy array
+        cob_arr = np.array(offset_to_cob[offset])
+        if y_correction: correct_y(cob_arr, normalize_to_bl)
+        # Turn com results into numpy array
+        com_arr = np.array(offset_to_com[offset])
+        if y_correction: correct_y(com_arr, normalize_to_bl)
+        # Compute delta
+        delta_arr = com_arr - cob_arr
+        if normalize_delta:
+            # Normalize delta according to the sizes of the bounding boxes
+            box_widths = np.array([metrics_dict[(str(k), offset)]['box_width'] for k in good_ks_dict[offset]])
+            box_heights = np.array([metrics_dict[(str(k), offset)]['box_height'] for k in good_ks_dict[offset]])
+            delta_arr[:,0] *= 1/box_widths/2
+            delta_arr[:,1] *= 1/box_heights/2
+        # Plot cob x
+        ax_xs_delta.plot(good_ks_dict[offset], delta_arr[:,0], linestyle, markersize=markersize, color=color_cob)
+        # Plot cob y
+        ax_ys_delta.plot(good_ks_dict[offset], delta_arr[:,1], linestyle, markersize=markersize, color=color_cob)
+    # Plot the xs and ys of com
+    for offset, color_com in zip(sorted_offsets, colors_com):
+        # Turn com results into numpy array
+        com_arr = np.array(offset_to_com[offset])
+        if y_correction: correct_y(com_arr, normalize_to_bl)
+        # Plot com x
+        ax_xs.plot(good_ks_dict[offset], com_arr[:,0], linestyle, markersize=markersize, color=color_com)
+        # Plot com y
+        ax_ys.plot(good_ks_dict[offset], com_arr[:,1], linestyle, markersize=markersize, color=color_com)
+    # Label everything
+    fig.suptitle("Center of Mass (COM) vs Delta from Center of Bounding Box (COBB)")
+    ax_xs.set_title("COM X Position")
+    ax_ys.set_title("COM Y Position")
+    if normalize_to_bl:
+        y_units = " (body-lengths)"
+    else:
+        y_units = " (m)"
+    for ax in [ax_xs, ax_ys]:
+        ax.set_ylabel("Position"+y_units)
+        ax.set_xlabel("k")
+    ax_xs_delta.set_title("Delta X from COBB to COM")
+    ax_ys_delta.set_title("Delta Y from COBB to COM")
+    if normalize_delta:
+        y_units = " (Proportion of Half Bounding Box)"
+    for ax in [ax_xs_delta, ax_ys_delta]:
+        ax.set_ylabel("Position"+y_units)
+        ax.set_xlabel("k")
+    # Set up rightmost axes as colorbars
+    # Remove ticks
+    for ax, colors in zip([ax_color_com, ax_color_cob], [colors_com, colors_cob]):
+        # Remove ticks
+        ax.set_xticks([])
+        ax.yaxis.tick_right()
+        ax.set_yticks([0,19])
+        ax.set_yticklabels([str(offset) for offset in [sorted_offsets[-1], sorted_offsets[0]] ], minor=False)
+
+        # set up the color bars as images
+        colors_np = np.flip(np.expand_dims(np.array(colors), axis=1), axis=0)
+        ax.imshow(colors_np)
+        ax.set_title("Offset")
+    if save_dir is not None:
+        fig.savefig(save_dir+'cob-com.png')
+    if show:
+        plt.show()
+    return fig, (ax_xs, ax_ys)
+
+def plot_metric(metric_name:str, metrics_dict: Dict, ks: List, offsets: List, show: bool = True, save_dir: Optional[str] = None):
     offset_to_metric, good_ks_dict, sorted_ks, sorted_offsets = get_results_for_metric(metric_name, metrics_dict, ks, offsets)
     # Plot all of the ks and offsets with successful bridge formation
     cmap = cm.get_cmap('viridis')
     color_indicies = np.linspace(0.25, 0.75, len(offsets))
     colors = [cmap(color_index) for color_index in color_indicies]
     # print(sorted_offsets)
+    fig = plt.figure(figsize=(24, 13))
     for offset, color in zip(sorted_offsets, colors):
         plt.plot(good_ks_dict[offset], offset_to_metric[offset],'.:', color = color)
     plt.title("Results for "+metric_name)
     plt.legend(["Offset: "+ str(float(offset)/1.02) for offset in sorted_offsets])
     plt.xlabel("K")
     plt.ylabel(metric_name)
-    plt.show()
+    if save_dir is not None:
+        fig.savefig(save_dir+metric_name+".png")
+    if show:
+        plt.show()
 
 def write_incomplete_dirs_to_yaml(incomplete_dirs: List[str], filepath: str)->None:
     import io
@@ -196,6 +274,9 @@ def write_incomplete_dirs_to_yaml(incomplete_dirs: List[str], filepath: str)->No
 parser = argparse.ArgumentParser(description="Process the sweep from the simulator.")
 parser.add_argument("directory", help="Directory where results are saved.")
 parser.add_argument("-m", "--metric", help="Metric to plot")
+parser.add_argument("-s", "--save", help="Directory to save images of plots in")
+parser.add_argument("-q", "--quiet", help="Don't show any plots", default=False)
+
 args = parser.parse_args()
 
 ks = []
@@ -223,13 +304,15 @@ no_result_dirs = []
 for root, dirs, files in os.walk(args.directory):
     if "_result.txt" not in files:
         no_result_dirs.append(root)
-        
+
 PP.pprint(no_result_dirs)
 
 if args.metric is None:
     for metric_name in METRICS:
-        plot_metric(metric_name, metrics_dict, ks, offsets)
-elif args.metric == "cob-com":
-    plot_cob_com_comparison(metrics_dict, ks, offsets)
+        plot_metric(metric_name, metrics_dict, ks, offsets, show = not args.quiet, save_dir = args.save)
+if args.metric is None or args.metric == "cob-com" or args.metric == "com-cob":
+    plot_cob_com_comparison(metrics_dict, ks, offsets, show = not args.quiet, save_dir = args.save)
+if args.metric is None or args.metric == "cob-com-delta" or args.metric == "com-cob-delta":
+    plot_cob_com_comparison_with_delta(metrics_dict, ks, offsets, show = not args.quiet, save_dir = args.save)
 else:
-    plot_metric(args.metric, metrics_dict, ks, offsets)
+    plot_metric(args.metric, metrics_dict, ks, offsets, show = not args.quiet, save_dir = args.save)
