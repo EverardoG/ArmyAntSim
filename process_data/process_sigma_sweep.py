aed9ce1ebed8a2ac02bbb31a0988c5251b1239dc
EverardoG <everardo.a.gonzalez@gmail.com>
2022-02-22 11:38:04 -0800

    4 panel sigma sweep generated

--- process_data/process_sigma_sweep.py
+++ process_data/process_sigma_sweep.py
@@ -12,8 +12,8 @@ from utils import get_metrics_from_folder
 PP = pprint.PrettyPrinter(indent=4)
 
 # Set global matplotlib parameters
-TICK_FONT_SIZE = 18
-AXES_FONT_SIZE = 23
+TICK_FONT_SIZE = 13
+AXES_FONT_SIZE = 18
 FACE_COLOR = 0.91
 GRID_COLOR = 0.98
 MARKER_SIZE = 10
@@ -28,14 +28,22 @@ plt.rc('mathtext', fontset='stix')
 plt.rc('font', family='STIXGeneral')
 
 TERRAIN_LABELS = {
-    "pit" : "Bridge",
-    "terrace": "Cantilever",
-    "stepdown": "Ramp",
-    "island": "Tower"
+    "pit" : "Canyon",
+    "terrace": "Cliff",
+    "stepdown": "Overhang",
+    "island": "Island"
 }
 
 # Get colors for plotting and legend
 COLOR_CYCLE = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+# Set constants for labelling things
+FORMATION = 0
+TRAVEL = 1
+
+STD_DEV = 0
+FULL_RANGE = 1
+NONE = 3
 
 def getTerrainSigma(ts_path: str):
     terrain = ts_path.split("_")[0]
@@ -74,7 +82,7 @@ def stylizePlot(ax, xlabels=Optional[List[float]]):
     return None
 
 
-def calculateSuccessRates(all_results, terrains, sigmas):
+def calculateSuccessRates(all_results, terrains, sigmas, which):
     # key is terrain. Value is a list of percentage formation success in order of sigmas
     success_rates_dict = {}
 
@@ -82,21 +90,22 @@ def calculateSuccessRates(all_results, terrains, sigmas):
         success_rate_for_terrain = []
         for sigma in sigmas:
             trials_results = all_results[(terrain, sigma)]
-            formation_times = [t["formation_time"] for t in trials_results]
-            num_successful_formations = [ft is not None and ft != 0.0 for ft in formation_times]
-            success_rate = sum(num_successful_formations)/len(num_successful_formations)
+            if which == FORMATION:
+                times = [t["formation_time"] for t in trials_results]
+            elif which == TRAVEL:
+                times = [t["travel_time"] for t in trials_results]
+            num_successful = [ft is not None and ft != 0.0 for ft in times]
+            success_rate = sum(num_successful)/len(num_successful)
             success_rate_for_terrain.append(success_rate)
         success_rates_dict[terrain] = success_rate_for_terrain
 
     return success_rates_dict
 
-def plotSuccesRates(all_results, terrains, sigmas):
+def plotSuccesRates(ax, all_results, terrains, sigmas, which):
     # Grab all the success rates
-    success_rates_dict = calculateSuccessRates(all_results, terrains, sigmas)
+    success_rates_dict = calculateSuccessRates(all_results, terrains, sigmas, which)
     # Convert sigmas to floats
     sigmas_f = [float(s) for s in sigmas]
-    # Create an Axes object for plotting on
-    fig, ax = plt.subplots(1,1,dpi=100,figsize=(12, 8))
     # Plot the success rate for each terrain
     for terrain, c in zip(terrains, COLOR_CYCLE[:len(terrains)]):
         ax.plot(sigmas_f, success_rates_dict[terrain],'.-', color=c, markersize=MARKER_SIZE)
@@ -105,15 +114,22 @@ def plotSuccesRates(all_results, terrains, sigmas):
     terrain_legend = [TERRAIN_LABELS[terrain] for terrain in terrains]
     ax.legend(custom_legend_handles, terrain_legend)
     # Create labels for x and y
-    ax.set_ylabel("Formation Sucess Rate")
+    if which == FORMATION:
+        ax.set_ylabel("Formation Sucess Rate")
+    elif which == TRAVEL:
+        ax.set_ylabel("Utility Sucess Rate")
     ax.set_xlabel(r'$\sigma$'+" Noise (BL)")
     # Stylize plot so it looks nice
     stylizePlot(ax, sigmas_f)
-    # Remove excess whitespace
-    fig.tight_layout()
-    plt.show()
+    return None
 
-def calculateFormationTimeStatisitics(all_results, terrains, sigmas):
+def plotFormationSuccessRates(ax, all_results, terrains, sigmas):
+    return plotSuccesRates(ax, all_results, terrains, sigmas, FORMATION)
+
+def plotTravelSucessRates(ax, all_results, terrains, sigmas):
+    return plotSuccesRates(ax, all_results, terrains, sigmas, TRAVEL)
+
+def calculatePhaseTimeStatisitics(all_results, terrains, sigmas, which):
     # Key is terrain. Value is a list of times in order of sigmas
     averages = {}
     uppers = {}
@@ -127,13 +143,16 @@ def calculateFormationTimeStatisitics(all_results, terrains, sigmas):
         std_deviations_for_terrain = []
         for sigma in sigmas:
             trials_results = all_results[(terrain, sigma)]
-            formation_times = [t["formation_time"] for t in trials_results]
-            valid_formation_times = [ft for ft in formation_times if ft is not None and ft != 0.0]
-            if len(valid_formation_times) > 0:
-                averages_for_terrain.append(np.average(valid_formation_times))
-                uppers_for_terrain.append(np.max(valid_formation_times))
-                lowers_for_terrain.append(np.min(valid_formation_times))
-                std_deviations_for_terrain.append(np.std(valid_formation_times))
+            if which == FORMATION:
+                times = [t["formation_time"] for t in trials_results]
+            elif which == TRAVEL:
+                times = [t["travel_time"] for t in trials_results]
+            valid_times = [t for t in times if t is not None and t != 0.0]
+            if len(valid_times) > 0:
+                averages_for_terrain.append(np.average(valid_times))
+                uppers_for_terrain.append(np.max(valid_times))
+                lowers_for_terrain.append(np.min(valid_times))
+                std_deviations_for_terrain.append(np.std(valid_times))
             else:
                 averages_for_terrain.append(None)
                 uppers_for_terrain.append(None)
@@ -174,38 +193,60 @@ def getValidXs(xs: List[float], ys: List[float or None]):
             valid_xs.append(x)
     return valid_xs
 
-def plotFormationTimes(all_results, terrains, sigmas, plot_std_dev=True):
+def plotTimeStatistics(ax, all_results, terrains, sigmas, which, plot_range):
     # Grab all the formation times
-    averages, uppers, lowers, std_deviations = calculateFormationTimeStatisitics(all_results, terrains, sigmas)
+    averages, uppers, lowers, std_deviations = calculatePhaseTimeStatisitics(all_results, terrains, sigmas, which)
     # Convert sigmas to floats
     sigmas_f = [float(s) for s in sigmas]
-    # Create axes object
-    fig, ax = plt.subplots(1,1,dpi=100,figsize=(12,8))
     # Plot the formation times for each terrain (Mean and standard deviation)
     for terrain, c in zip(terrains, COLOR_CYCLE[:len(terrains)]):
         valid_sigmas = getValidXs(sigmas_f, averages[terrain])
         avg = filterOutNone(averages[terrain])
-        if plot_std_dev:
+        ax.plot(valid_sigmas, avg, '.-', color=c, markersize=MARKER_SIZE)
+        if plot_range == STD_DEV:
             std_dev = filterOutNone(std_deviations[terrain])
             upper_std_dev = np.array(avg) + np.array(std_dev)
             lower_std_dev = np.array(avg) - np.array(std_dev)
-            ax.plot(valid_sigmas, avg, '.-', color=c, markersize=MARKER_SIZE)
             ax.fill_between(valid_sigmas,lower_std_dev,upper_std_dev,alpha=0.25)
-        else: # Plot upper and lower ranges if specified
+        elif plot_range == FULL_RANGE: # Plot upper and lower ranges if specified
             up = filterOutNone(uppers[terrain])
             low = filterOutNone(lowers[terrain])
             ax.fill_between(valid_sigmas, up, low, alpha=0.2)
+        elif plot_range == NONE:
+            pass
     # Create a legend for the terrains
     custom_legend_handles = [Line2D([0],[0], color=c, marker='o', linestyle='', markersize=LEGEND_MARKER_SIZE) for c in COLOR_CYCLE[:len(terrains)]]
     terrain_legend = [TERRAIN_LABELS[terrain] for terrain in terrains]
     ax.legend(custom_legend_handles, terrain_legend)
+    # Create labels for x and y
+    if which == FORMATION:
+        ax.set_ylabel("Formation Time (s)")
+    elif which == TRAVEL:
+        ax.set_ylabel("Utility Time (s)")
+    ax.set_xlabel(r'$\sigma$'+" Noise (BL)")
+    # Set y limits to make plot more readable
+    ax.set_ylim([0,500])
     # Stylize plot so it looks nice
     stylizePlot(ax, sigmas_f)
-    # Remove excess whitespace
+    return None
+
+def plotFormationTimes(ax, all_results, terrains, sigmas, plot_range=STD_DEV):
+    return plotTimeStatistics(ax, all_results, terrains, sigmas, FORMATION, plot_range)
+
+def plotTravelTimes(ax, all_results, terrains, sigmas, plot_range=STD_DEV):
+    return plotTimeStatistics(ax, all_results, terrains, sigmas, TRAVEL, plot_range)
+
+def generateSigmaFigure():
+    fig, axs = plt.subplots(1,4,dpi=100,figsize=(24,4))
+    plotFormationSuccessRates(axs[0], all_results, terrains, sigmas)
+    plotTravelSucessRates(axs[1],all_results, terrains, sigmas)
+    plotFormationTimes(axs[2], all_results, terrains, sigmas)
+    plotTravelTimes(axs[3], all_results, terrains, sigmas)
     fig.tight_layout()
-    plt.show()
+    fig.subplots_adjust(left=0.04)
+    return fig, axs
 
 folder_path = "/media/egonzalez/Extreme SSD/FlippybotsData/sweep_iros_terrains_sigma_0_to_10"
 all_results, terrains, sigmas = getAllResults(folder_path, populate_robots_at_times=False)
-# plotSuccesRates(all_results, terrains, sigmas)
-plotFormationTimes(all_results, terrains, sigmas)
+fig, axs = generateSigmaFigure()
+plt.show()
